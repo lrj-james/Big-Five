f51eaa5b25775f199de6c911a4afee6b400166dc
James Li <james098093@gmail.com>
2024-08-17 23:41:55 +0800

    整理 app.py 程式碼

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import sqlite3
 from flask import Flask, render_template, request
+import sqlite3
+import random
 import plotly.graph_objs as go
 import plotly.io as pio
 import base64
@@ -20,9 +21,8 @@ traits = {
 
 
 # 製作題目列表，共25題隨機排列
-def make_qlist(traits):
+def make_qlist():
     
-    import random
     qlist = []
     
     # 取出字典中的人格縮寫，與題號共同組成4位編號
@@ -36,8 +36,65 @@ def make_qlist(traits):
     return qlist  
 
 
+def generate_questions():
+
+    qlist = make_qlist()  
+    
+    # 依據隨機的題目列表從資料庫匯入題庫
+    con = sqlite3.connect("questions.db")
+    cur = con.cursor()
+    
+    rows = []
+    for index_code in qlist:
+        rows.append(cur.execute(
+            'SELECT index_code, question, score FROM questions WHERE index_code = (?)', (index_code,)
+        ).fetchone())
+    
+    con.close()
+    
+    # 將題庫從串列型態轉為字典
+    # count為題號
+    # index_code 為題目的4位編號
+    # questions為題目字串本身
+    # score表示該題表示該人格的加分或減分
+    global questions
+    questions = []
+    count = 1
+    for row in rows:
+        new_row = {}
+        new_row['count'] = count
+        new_row['index_code'] = row[0]
+        new_row['question'] = row[1]
+        new_row['score'] = row[2]
+        questions.append(new_row)
+        count += 1
+        
+    return questions
+
+
+# 計算測驗結果
+def calc_result():
+    
+    # 接收使用者的回答
+    for i in range(1, 26):
+        questions[i-1]['choice'] = (request.form.get('q' + str(i) + '_choice'))
+        
+    # 計分
+    for row in questions:
+        if (
+            row['score'] == '+' and row['choice'] == 'y'
+        ) or (
+            row['score'] == '-' and row['choice'] == 'n'
+            
+        ):
+            traits[row['index_code'][:2]] += 1
+        else:
+            traits[row['index_code'][:2]] -= 1
+
+
 # 生成雷達圖
-def generate_radar_chart(traits):
+## TODO: 中文字型顯示錯誤
+def generate_radar_chart():
     
     categories = [
         '開放性 (Openness)', 
@@ -61,7 +118,7 @@ def generate_radar_chart(traits):
             polar = {'radialaxis': {'range': [-5, 5], 'visible': True}},
             showlegend = True,
             font = dict(
-                family = "SimHei, Arial",
+                family = "SimHei, Arial", # GPT建議，無效
                 size = 14
             )
         )
@@ -73,69 +130,30 @@ def generate_radar_chart(traits):
     img_buffer.seek(0)
 
     # 將圖片轉換為 Base64 字串
-    img_str = base64.b64encode(img_buffer.read()).decode('utf-8')
+    src = base64.b64encode(img_buffer.read()).decode('utf-8')
 
-    return img_str
+    return src
 
  
 @app.route("/", methods=['GET', 'POST'])
 def index():
     
-    global questions
-    
     if request.method == "GET":
     
-        # 歸零變數
+        # 歸零人格特質變數
         for trait in traits:
             traits[trait] = 0
 
-        # 隨機挑選題目
-        global qlist
-        qlist = make_qlist(traits)  
-        
-        # 匯入題庫
-        con = sqlite3.connect("questions.db")
-        cur = con.cursor()
-        
-        rows = []
-        for index_code in qlist:
-            rows.append(cur.execute(
-                'SELECT index_code, question, score FROM questions WHERE index_code = (?)', (index_code,)
-            ).fetchone())
-        
-        con.close()
-        
-        questions = []
-        count = 1
-        for row in rows:
-            new_row = {}
-            new_row['count'] = count
-            new_row['index_code'] = row[0]
-            new_row['question'] = row[1]
-            new_row['score'] = row[2]
-            questions.append(new_row)
-            count += 1
+        questions = generate_questions()
 
         return render_template("index.html", questions=questions)
     
     # 傳送表單則計算測驗結果
     else:
         
-        for i in range(1, 26):
-            questions[i-1]['choice'] = (request.form.get('q' + str(i) + '_choice'))
-            
-        for row in questions:
-            if (
-                row['score'] == '+' and row['choice'] == 'y'
-            ) or (
-                row['score'] == '-' and row['choice'] == 'n'
+        calc_result()
                 
-            ):
-                traits[row['index_code'][:2]] += 1
-            else:
-                traits[row['index_code'][:2]] -= 1
-                
-        src = generate_radar_chart(traits)
+        src = generate_radar_chart()
         
         return render_template('result.html', traits=traits, src=src)
     
